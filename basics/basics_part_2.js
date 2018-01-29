ec791548a690d6fc7f4b8b543bd3c38911e3e6d1
MatysApiros <matheus.canabarro@hotmail.com>
2018-01-29 10:27:00 -0200

    Basics part 3 added

--- basics/basics_part_2.js
+++ basics/basics_part_2.js
@@ -2,13 +2,15 @@ function openSheetById(){
     var ss = SpreadsheetApp.openById("12xe0aBxeAq3GtQOpFh2Ih8L0oYqdfwULvbEzCvzO3-Y");
     return ss;
   }
-  
+
+  // Set a value to a row
 function setValueSheet(){
     var ss = openSheetById();
     var sheet = ss.getActiveSheet();
     sheet.getRange("C1").setValue("Hello World!");
   }
 
+//   Get the data from the active Sheet
   function getLogSheet(){
     var ss = openSheetById();
     var sheet = ss.getActiveSheet();
---
+++ basics/basics_part_3.js
@@ -0,0 +1,52 @@
+//  Log Function
+function logPanel() {
+    Logger.log("Hello World!");
+  }
+  
+//   Set method with different 'getRange' parameters
+  function learnBasics2() {
+    var app = SpreadsheetApp;
+    var ss = app.getActiveSpreadsheet();
+    var activeSheet = ss.getActiveSheet();
+    
+    activeSheet.getRange("A1").setValue("Hello World!");
+    
+    activeSheet.getRange("D7:F1").setValue("Hello World!");
+    
+    activeSheet.getRange(2, 2, 3).setValue("Hello World!");
+    
+    activeSheet.getRange(1,8,3,3).setValue("Hello World!");
+    
+    var text = activeSheet.getRange(1,1).getValue();
+    activeSheet.getRange(6,2).setValue(text);
+  }
+
+// Add + n to a row
+  function learnBasics3(){
+    var app = SpreadsheetApp;
+    var activeSheet = app.getActiveSpreadsheet().getActiveSheet();
+    var someCell = activeSheet.getRange(8,1).getValue();
+    activeSheet.getRange(8,1).setValue(someCell + 5);
+  }
+  
+// Simple Loop Method
+  function simpleForLoop(){
+    var app = SpreadsheetApp;
+    var activeSheet = app.getActiveSpreadsheet().getActiveSheet();
+    
+    for(i = 8; i < 12; i++){
+      var someCell = activeSheet.getRange(i+8,1).getValue();
+      someCell = someCell + 5;
+      activeSheet.getRange(i,1).setValue(someCell);
+    }
+  }
+
+// Copy data to other other sheet
+  function copyToOtherSheet(){
+    var app = SpreadsheetApp;
+    var targetSheet = app.getActiveSpreadsheet().getSheetByName("Página2");
+    var tempNumber = targetSheet.getRange(2,1).getValue();
+    var secondSheet = app.getActiveSpreadsheet().getSheetByName("Página3");
+    
+    secondSheet.getRa;
+  }
